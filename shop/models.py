c0993443e81a4b4d91877b9642e70691239ee033
zaripov4 <zaripovulugbek45@gmail.com>
2024-04-03 18:00:56 +0500

    project dockerized

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Shop(models.Model):
     title = models.CharField(_("Shop Title"), max_length=255)
     description = models.CharField(_("Description"), blank=True, max_length=255)
@@ -8,29 +9,32 @@ class Shop(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         verbose_name = _("Shop")
         verbose_name_plural = _("Shops")
 
+
 class Category(models.Model):
     title = models.CharField(_("Category Name"), max_length=255)
-    parent_category = models.ForeignKey("self", on_delete = models.CASCADE, null=True, blank=True, related_name="children")
-
+    parent_category = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
 
     def __str__(self):
         return self.title
-    
+
     def get_all_parents(self):
         parents = [self]
         while self.parent_category:
             self = self.parent_category
             parents.append(self)
         return parents[::-1]
-    
+
     class Meta:
         verbose_name = _("Category")
         verbose_name_plural = _("Categories")
+
 
 class Product(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="products")
@@ -43,19 +47,22 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         verbose_name = _("Product")
         verbose_name_plural = _("Products")
 
+
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete = models.CASCADE, related_name = "images")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
     image = models.ImageField(_("Product Image"), upload_to="product_images/")
     is_main = models.BooleanField(_("Main Image"), default=False)
 
     def __str__(self):
         return f"{self.product.title} - Image {self.pk}"
-    
+
     class Meta:
         verbose_name = _("Product Image")
         verbose_name_plural = _("Product Images")
